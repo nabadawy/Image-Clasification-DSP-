11fae62cb41f7ff2afeb6704b67df3f23dd5306e
nabadawy <62911632+nabadawy@users.noreply.github.com>
2020-11-30 21:31:30 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,30 @@
-import os
-import sys
-import numpy as np
+
 import matplotlib.pyplot as plt
+import numpy as np
+import cv2
+
+#img = plt.imread('images/1.jpg')/float(2**8)
+
+
+
+
+def low_pass_filter(img_in):  # Write low pass filter here
+    dft = cv2.dft(np.float32(img_in), flags=cv2.DFT_COMPLEX_OUTPUT)
+    dft_shift = np.fft.fftshift(dft)
+    magnitude_spectrum = 20 * np.log(
+    cv2.magnitude(dft_shift[:, :, 0], dft_shift[:, :, 1]))
+    rows, cols = img_in.shape
+    crow, ccol = rows / 2, cols / 2
+    mask = np.zeros((rows, cols, 2), np.uint8)
+    mask[crow - 10: crow + 10, ccol - 10: ccol + 10] = 1
+    fshift = dft_shift * mask
+    f_ishift = np.fft.ifftshift(fshift)
+    img_back = cv2.idft(f_ishift, flags=cv2.DFT_SCALE | cv2.DFT_REAL_OUTPUT)
+    img_out = img_back
+    return img_out
+
+input_image1 = cv2.imread('images/1.jpg', 0)
+# Low and high pass filter
+output_image1 = low_pass_filter(input_image1)
+#succeed2, output_image2 = high_pass_filter(input_image1)
+cv2.imwrite("output_name1", output_image1)
